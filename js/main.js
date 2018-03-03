0e909bf1489a0a440072afc0dbf63aec1d5a3b3c
rom4es <rom4es_1993@mail.ru>
2018-03-03 14:20:48 +0700

    123

--- js/main.js
+++ js/main.js
@@ -121,12 +121,6 @@ $(document).ready(function(){
         $(".b-photo-slider").slick('slickNext');
     });
 
-    /*if( !isMobile && !isSmallTablet && $('.stick').length) {
-        $(window).load(function(){
-            $(".stick").stick_in_parent();  
-        }); 
-    }*/
-
     $(".b-side-right").stick_in_parent({offset_top: 24});
     
 	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
